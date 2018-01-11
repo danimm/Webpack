0523159ebfa5264fe204a7945978beb14e9b35d3
Daniel Muñoz Martín <27631697+danimm@users.noreply.github.com>
2018-01-11 08:42:20 +0100

    Optimizando el paquete de dependencias comunes

---
+++ dll/src/js/index.js
@@ -0,0 +1,30 @@
+import '../css/estilos.css'
+import { firstMessage,delayedMessage } from './message.js'
+import platziImg from '../images/platzi.png'
+import data from './teachers.json'
+import renderToDOM from './render-to-dom'
+
+import React from 'react'
+import { render } from 'react-dom'
+import Teachers from './components/teachers'
+import '../css/main.less'
+
+render(<Teachers data={data} />, document.getElementById('container'))
+
+console.log(data)
+
+// data.teachers.forEach((teacher) => {
+//   const element = document.createElement('li')
+//   element.textContent = teacher.name
+//   renderToDOM(element)
+// })
+
+document.write(firstMessage)
+delayedMessage()
+
+const img = document.createElement('img')
+img.setAttribute('src', platziImg)
+img.setAttribute('width', 50)
+img.setAttribute('height', 50)
+document.body.append(img)
+console.log('Hola mundo!, desde Webpack.config!')
---
+++ dll/webpack.dll.config.js
@@ -0,0 +1,126 @@
+const path = require('path')
+const extractTextPlugin = require('extract-text-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    modules: [
+      'react',
+      'react-dom',
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    library: '[name]'
+  },
+  // module: {
+  //   rules: [
+  //     //Aquí van los loaders
+  //     {
+  //       // test: que tipo de archivo quiero reconocer,
+  //       // use: que loader se va a encargar del archivo
+  //       test: /\.js$/,
+  //       use: {
+  //         loader: 'babel-loader',
+  //         options: {
+  //           presets: ['es2015', 'react']
+  //         }
+  //       }
+  //     },
+  //     {
+  //       test: /\.json$/,
+  //       use: 'json-loader'
+  //     },
+  //     {
+  //       test: /\.(jpg|png|gif|woff|eot|ttf|svg)$/,
+  //       use: {
+  //         loader: 'url-loader',
+  //         options: {
+  //           limit: 100000
+  //         }
+  //       }
+  //     },
+  //     {
+  //       // test: que tipo de archivo quiero reconocer,
+  //       // use: que loader se va a encargar del archivo
+  //       test: /\.css$/,
+  //       use:extractTextPlugin.extract({
+  //         // ['style-loader','css-loader']
+  //         // fallback: 'style-loader',
+  //         use: [
+  //           {
+  //             loader: 'css-loader',
+  //             options: {
+  //               modules: true,
+  //               importLoaders: 1
+  //             }
+  //           },
+  //           'postcss-loader'
+  //         ]
+  //       })
+  //     },
+  //     {
+  //       // test: que tipo de archivo quiero reconocer,
+  //       // use: que loader se va a encargar del archivo
+  //       test: /\.scss$/,
+  //       use:extractTextPlugin.extract({
+  //         // ['style-loader','css-loader']
+  //         fallback: 'style-loader',
+  //         use: ["css-loader", "sass-loader"]
+  //       })
+  //     },
+  //     {
+  //       // test: que tipo de archivo quiero reconocer,
+  //       // use: que loader se va a encargar del archivo
+  //       test: /\.less$/,
+  //       use:extractTextPlugin.extract({
+  //         // ['style-loader','css-loader']
+  //         fallback: 'style-loader',
+  //         use: ["css-loader", {
+  //           loader: 'less-loader',
+  //           options: {
+  //             noIeCompat: true
+  //           }
+  //         }]
+  //       })
+  //     },
+  //     {
+  //       // test: que tipo de archivo quiero reconocer,
+  //       // use: que loader se va a encargar del archivo
+  //       test: /\.styl$/,
+  //       use:extractTextPlugin.extract({
+  //         // ['style-loader','css-loader']
+  //         fallback: 'style-loader',
+  //         use: [
+  //           "css-loader",
+  //           {
+  //             loader: 'stylus-loader',
+  //             options: {
+  //               use: [
+  //                 require('nib'),
+  //                 require('rupture')
+  //               ],
+  //               import: [
+  //                 '~nib/lib/nib/index.styl',
+  //                 '~rupture/rupture/index.styl',
+  //             ]
+  //           }
+  //         }]
+  //       })
+  //     }
+  //   ]
+  // },
+  plugins: [
+    // aquí van los plugins
+    // new extractTextPlugin("css/[name].css"),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: 'vendor',
+    //   minChunks: Infinity,
+    // })
+    new webpack.DllPlugin({
+      name: "[name]",
+      path: path.join(__dirname, "[name]-manifest.json")
+    })
+  ]
+}
